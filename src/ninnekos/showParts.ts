5eef871172637f5c2509cbe8be7b893a164abdeb
André Lopes <andrevctr12@gmail.com>
2022-03-18 23:10:05 -0300

    Fix show function

--- src/ninnekos/showParts.ts
+++ src/ninnekos/showParts.ts
@@ -1,23 +1,27 @@
-import { CommandInteraction } from "discord.js";
-import { parts } from "../parts";
+import { CommandInteraction } from 'discord.js';
+import { parts } from '../parts';
 import { AsciiTable3, AlignmentEnum } from 'ascii-table3';
 
-export default async function showParts(type: number, interaction: CommandInteraction) {
-    let printable = Object.values(parts).filter(
-        (obj) => obj.part === type
-    ).map(part => part.name);
-    console.log(printable);
+export default async function showParts(
+    type: number,
+    interaction: CommandInteraction
+) {
+    let printable = Object.values(parts)
+        .filter((obj) => obj.part === type)
+        .map((part) => part.name);
 
-    const table = new AsciiTable3()
-        .setAligns(AlignmentEnum.LEFT)
-        .setStyle('none');
-    
+    const table = new AsciiTable3();
+
     if (printable?.length) {
         for (let i = 0; i <= printable.length; i += 3) {
-            table.addRowMatrix([printable?.[i], printable?.[i + 1], printable?.[i + 2]]);
+            table.addRowMatrix([
+                printable?.[i],
+                printable?.[i + 1],
+                printable?.[i + 2],
+            ]);
         }
     }
-    console.log(table.toString())
+    table.setAligns(AlignmentEnum.LEFT).setStyle('none');
 
     interaction.editReply(`\`\`\`${table.toString()}\`\`\``);
 }
